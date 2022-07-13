d3b8568ed23f2bfda85c59eb22347e676ab50667
Tagir Gumerov <gtagir@mail.ru>
2022-07-13 10:13:04 +0300

    С 4-ым уроком, крестики-нолики готовы

--- lesson4/HomeWorkApp.java
+++ lesson4/HomeWorkApp.java
@@ -1,12 +1,12 @@
 package lesson4;
 
-import java.lang.reflect.MalformedParameterizedTypeException;
-import java.util.Map;
+import java.util.Arrays;
 import java.util.Random;
 import java.util.Scanner;
 
 public class HomeWorkApp {
     private static final int SIZE=3;
+    private static final int SIZEWIN=3;
 
     private static final char DOT_EMPTY = '•';
     private static final char DOT_HUMAN = 'X';
@@ -18,23 +18,33 @@ public class HomeWorkApp {
     private static Random random = new Random();
 
     private static int rowNumber, columnNumber;
-    private static int numbersCharsMap = 0;
+    private static int turnsCount;
 
     public static void main(String[] args) {
-        for (int i = 0; i < SIZE; i++) {
-            for (int j = 0; j < SIZE; j++) {
-                MAP[i][j] = DOT_EMPTY;
-            }
-
-        }
+        completionArrMapDotEmpty();
         map();
         playGame();
 
     }
 
+    private static void completionArrMapDotEmpty() {
+        for (int i = 0; i < SIZE; i++) {
+            for (int j = 0; j < SIZE; j++) {
+                MAP[i][j] = DOT_EMPTY;
+            }
+        }
+    }
+
     private static void map() {
         oneStringmap(MAP);
         printBodyMap(MAP);
+    }
+
+    private static void oneStringmap(char[][] map) {
+        System.out.print(HEADER_FIRST_SYMBOL + " ");
+        for (int i = 0; i < SIZE; i++) {
+            printMapNumber(i);
+        }
     }
 
     private static void printBodyMap(char[][] map) {
@@ -46,31 +56,26 @@ public class HomeWorkApp {
             }
         }
         System.out.println();
-    }
-
-    private static void oneStringmap(char[][] map) {
-        System.out.print(HEADER_FIRST_SYMBOL + " ");
-        for (int i = 0; i < SIZE; i++) {
-            printMapNumber(i);
-        }
+        System.out.println();
     }
 
     private static void printMapNumber(int i) {
-        System.out.print(i +1 + " ");
+        System.out.print(i + 1 + " ");
     }
 
     private static void playGame() {
         while (true) {
             vvodKoordinatIgrokom();
-            checkEndHuman();
+            if (checkEnd(DOT_HUMAN)){
+                break;
+            }
 
             vvodKoordinatAI();
-            checkEndAI();
+            if (checkEnd(DOT_AI)){
+                break;
+            }
         }
 
-    }
-
-    private static void checkEndHuman() {
     }
 
     private static void  vvodKoordinatIgrokom() {
@@ -84,8 +89,8 @@ public class HomeWorkApp {
             else break;
         }
         MAP[rowNumber][columnNumber] = DOT_HUMAN;
-        numbersCharsMap++;
         map();
+        turnsCount++;
     }
 
     private static void vvodStolbcaHuman() {
@@ -97,6 +102,94 @@ public class HomeWorkApp {
             }
             else break;
         } while (true);
+    }
+
+    private static boolean checkEnd(char symbol) {
+        if (checkWin(symbol)) {
+            if (symbol == DOT_HUMAN) {
+                System.out.println("УРА ВЫ ПОБЕДИЛИ!");
+            } else {
+                System.out.println("Восстание близко... ИИ победил...");
+            }
+            return true;
+        }
+        return checkNichya();
+ //   }
+ //       return false;
+    }
+
+    private static boolean checkWin(char symbol) {
+        checkHorizontWin(symbol);
+        checkVertikalWin(symbol);
+        checkDiagonal1(symbol);
+        checkDiagonal2(symbol);
+        return false;
+    }
+
+    // rowNumber columnNumber
+    private static void checkDiagonal1(char symbol) {
+//        for (int i = 0; i < MAP.length; i++) {
+//            for (int j = 0; j < MAP[i].length; j++) {
+//                if (i == j || i+j+1 == ARRAY_SIZE){
+//                    arr[i][j] = 1;
+//                }
+//                System.out.printf("%2d ",arr[i][j]);
+//            }
+//            System.out.println();
+//        }
+    }
+
+    private static void checkDiagonal2(char symbol) {
+    }
+
+    private static boolean checkHorizontWin(char symbol) {
+        int n = 0;
+        for (int i = 0; i < SIZE; i++) {
+            if (MAP[rowNumber][i] == symbol) {
+                System.out.println(symbol);
+                n++;
+                if (n==SIZEWIN){
+                    if (symbol == DOT_HUMAN) {
+                        System.out.println("УРА ВЫ ПОБЕДИЛИ!");
+                    } else {
+                        System.out.println("Восстание близко... ИИ победил...");
+                    }
+                    return true;
+                }
+            }
+            else n = 0;
+        }
+        return false;
+    }
+
+    private static boolean checkVertikalWin(char symbol) {
+        int m = 0;
+        for (int i = 0; i < SIZE; i++) {
+            if (MAP[i][columnNumber] == symbol) {
+                m++;
+                if (m==SIZEWIN){
+                    if (symbol == DOT_HUMAN) {
+                        System.out.println("УРА ВЫ ПОБЕДИЛИ!");
+                    } else {
+                        System.out.println("Восстание близко... ИИ победил...");
+                    }
+                    return true;
+                }
+            }
+            else m = 0;
+        }
+        return false;
+    }
+
+    private static boolean checkNichya() {
+        if (checkDraw()) {
+            System.out.println("Ничья!");
+            return true;
+        }
+        return false;
+    }
+    private static boolean checkDraw() {
+        return turnsCount == SIZE * SIZE;
     }
 
     private static void vvodStrokiHuman() {
@@ -112,18 +205,15 @@ public class HomeWorkApp {
 
     private static void vvodKoordinatAI() {
        while (true) {
-           rowNumber = (int) (Math.random() * 3);
-           columnNumber = (int) (Math.random() * 3);
+           rowNumber = (int) (Math.random() * SIZE);
+           columnNumber = (int) (Math.random() * SIZE);
         if (MAP[rowNumber][columnNumber] == DOT_EMPTY){
             break;
         }
        }
             MAP[rowNumber][columnNumber] = DOT_AI;
-            numbersCharsMap++;
             map();
-    }
-
-    private static void checkEndAI() {
+        turnsCount++;
     }
 
     private static int getIntNumFromScanner() {
